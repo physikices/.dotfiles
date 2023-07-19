35d73f54eca1c0de723ec2ea2da9081b7b767e0b
Rodrigo Nascimento <rodrigo.nascimento@edu.udesc.br>
2023-07-18 22:14:55 -0300

    style: git scheme colors

--- .p10k.zsh
+++ .p10k.zsh
@@ -357,8 +357,8 @@
   # Version control background colors.
   typeset -g POWERLEVEL9K_VCS_CLEAN_BACKGROUND=14
   typeset -g POWERLEVEL9K_VCS_MODIFIED_BACKGROUND=230
-  typeset -g POWERLEVEL9K_VCS_UNTRACKED_BACKGROUND=2
-  typeset -g POWERLEVEL9K_VCS_CONFLICTED_BACKGROUND=3
+  typeset -g POWERLEVEL9K_VCS_UNTRACKED_BACKGROUND=198
+  typeset -g POWERLEVEL9K_VCS_CONFLICTED_BACKGROUND=9
   typeset -g POWERLEVEL9K_VCS_LOADING_BACKGROUND=8
 
   # Branch icon. Set this parameter to '\UE0A0 ' for the popular Powerline branch icon.
