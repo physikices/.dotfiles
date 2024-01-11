17e7e8b9bdfb073bb442a8f109d560a4b07c0cd6
Rodrigo Nascimento <rodrigo.nascimento@edu.udesc.br>
2024-01-11 03:21:46 -0300

    feat: update neo-tree git-icons

--- nvim/lua/plugins/editor/neo-tree.lua
+++ nvim/lua/plugins/editor/neo-tree.lua
@@ -7,7 +7,10 @@ return {
     "MunifTanjim/nui.nvim",
     -- "3rd/image.nvim",
   },
+
+
   config = function ()
+    local icons = require("user-icons")
     vim.keymap.set('n', '<leader>e', ':Neotree toggle<CR>', {})
     require("neo-tree").setup({
       filesystem = {
@@ -15,6 +18,19 @@ return {
           visible = true, -- This is what you want: If you set this to `true`, all "hide" just mean "dimmed out"
           hide_dotfiles = false,
           hide_gitignored = false,
+        }
+      },
+      git_status = {
+        symbols = {
+          added = icons.git.LineAdded,
+          modified = icons.git.LineModified,
+          deleted = icons.git.LineRemoved,
+          renamed = icons.git.FileRenamed,
+          untracked = icons.git.FileUntracked,
+          ignored = icons.git.FileIgnored,
+          unstaged = icons.git.FileUnstaged,
+          staged = icons.git.FileStaged,
+          conflict = icons.git.FileConflict
         }
       }
     })
