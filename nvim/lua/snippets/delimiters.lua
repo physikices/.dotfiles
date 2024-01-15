ff848bd0626f698046748c4494363caf82a3e725
Rodrigo Nascimento <rodrigo.nascimento@edu.udesc.br>
2024-01-15 20:19:27 -0300

    feat: add tex snippets

--- nvim/lua/snippets/delimiters.lua
+++ nvim/lua/snippets/delimiters.lua
@@ -30,7 +30,6 @@ end
 
 -- Return snippet tables
 return {
-
   ls.add_snippets("tex", {
     -- LEFT/RIGHT PARENTHESES
     s(
@@ -48,6 +47,127 @@ return {
           f(function(_, snip)
             return snip.captures[1]
           end),
+          d(1, get_visual),
+        }
+      )
+    ),
+    -- LEFT/RIGHT SQUARE BRACES
+    s(
+      {
+        trig = "([^%a])l%[",
+        regTrig = true,
+        wordTrig = false,
+        snippetType="autosnippet"
+      },
+      fmta(
+        "<>\\left[<>\\right]",
+        {
+          f( function(_, snip)
+            return snip.captures[1]
+          end),
+          d(1, get_visual),
+        }
+      )
+    ),
+    -- LEFT/RIGHT CURLY BRACES
+    s(
+      {
+        trig = "([^%a])l%{",
+        regTrig = true,
+        wordTrig = false,
+        snippetType="autosnippet"
+      },
+      fmta(
+        "<>\\left\\{<>\\right\\}",
+        {
+          f(function(_, snip)
+            return snip.captures[1]
+          end),
+          d(1, get_visual),
+        }
+      )
+    ),
+    -- BIG PARENTHESES
+    s(
+      {
+        trig = "([^%a])b%(",
+        regTrig = true,
+        wordTrig = false,
+        snippetType="autosnippet"
+      },
+      fmta(
+        "<>\\big(<>\\big)",
+        {
+          f(function(_, snip)
+            return snip.captures[1]
+          end),
+          d(1, get_visual),
+        }
+      )
+    ),
+    -- BIG SQUARE BRACES
+    s(
+      {
+        trig = "([^%a])b%[",
+        regTrig = true,
+        wordTrig = false,
+        snippetType="autosnippet"
+      },
+      fmta(
+        "<>\\big[<>\\big]",
+        {
+          f(function(_, snip)
+            return snip.captures[1]
+          end),
+          d(1, get_visual),
+        }
+      )
+    ),
+    -- BIG CURLY BRACES
+    s(
+      {
+        trig = "([^%a])b%{",
+        regTrig = true,
+        wordTrig = false,
+        snippetType="autosnippet"
+      },
+      fmta(
+        "<>\\big\\{<>\\big\\}",
+        {
+          f(function(_, snip)
+            return snip.captures[1]
+          end ),
+          d(1, get_visual),
+        }
+      )
+    ),
+    -- ESCAPED CURLY BRACES
+    s(
+      {trig = "([^%a])\\%{",
+        regTrig = true,
+        wordTrig = false,
+        snippetType="autosnippet",
+        priority=2000
+      },
+      fmta(
+        "<>\\{<>\\}",
+        {
+          f(function(_, snip)
+            return snip.captures[1]
+          end ),
+          d(1, get_visual),
+        }
+      )
+    ),
+    -- LATEX QUOTATION MARK
+    s(
+      {
+        trig = "``",
+        snippetType="autosnippet"
+      },
+      fmta(
+        "``<>''",
+        {
           d(1, get_visual),
         }
       )
