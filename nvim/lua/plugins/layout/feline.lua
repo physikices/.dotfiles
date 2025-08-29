47e736fdd564160dccdcb7bb005e006fe636de0d
Rodrigo Nascimento <rodrigo.nascimento@edu.udesc.br>
2025-08-29 00:12:53 -0300

    style: fix feline statusbar and bufflerline highlights

--- nvim/lua/plugins/layout/feline.lua
+++ nvim/lua/plugins/layout/feline.lua
@@ -6,7 +6,7 @@ return {
   config = function ()
     local ctp_feline = require "catppuccin.groups.integrations.feline"
     require('feline').setup({
-      components = ctp_feline,
+      components = ctp_feline.get_statusline(),
       options = {
         theme = 'catppuccin',
       }
