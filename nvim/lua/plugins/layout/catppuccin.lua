c3f4422b3c39d1f4e9ff8ee5ff71c76ea134c192
Rodrigo Nascimento <rodrigo.nascimento@edu.udesc.br>
2026-03-16 11:00:14 -0300

    fix: colorscheme catppuccin-mocha

--- nvim/lua/plugins/layout/catppuccin.lua
+++ nvim/lua/plugins/layout/catppuccin.lua
@@ -1,24 +1,59 @@
 return {
-  {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    priority = 1000, -- carrega antes de outros temas/plugins
-    opts = {
-      flavour = "mocha", -- opcional: mocha, macchiato, frappe, latte
-      integrations = {
-        bufferline = true,
-        feline = true,
-        gitsigns = true,
-        notify = true,
-        telescope = true,
-        treesitter = true,
-        -- adicione outras integrações que use
+  "catppuccin/nvim",
+  lazy = false,
+  name = "catppuccin",
+  priority = 1000, -- carrega antes de outros temas/plugins
+  opts = {
+    flavour = "mocha",
+    lsp_styles = {
+      underlines = {
+        errors = { "undercurl" },
+        hints = { "undercurl" },
+        warnings = { "undercurl" },
+        information = { "undercurl" },
       },
     },
-    config = function(_, opts)
-      require("catppuccin").setup(opts)
-      vim.cmd.colorscheme("catppuccin")
-    end,
+    integrations = {
+      aerial = true,
+      alpha = true,
+      cmp = true,
+      dashboard = true,
+      flash = true,
+      fzf = true,
+      grug_far = true,
+      gitsigns = true,
+      headlines = true,
+      illuminate = true,
+      indent_blankline = { enabled = true },
+      leap = true,
+      lsp_trouble = true,
+      mason = true,
+      mini = true,
+      navic = { enabled = true, custom_bg = "lualine" },
+      neotest = true,
+      neotree = true,
+      noice = true,
+      notify = true,
+      snacks = true,
+      telescope = true,
+      treesitter_context = true,
+      which_key = true,
+    },
+  },
+  config = function(_, opts)
+    require("catppuccin").setup(opts)
+    vim.cmd.colorscheme("catppuccin-mocha")
+  end,
+  specs = {
+    {
+      "akinsho/bufferline.nvim",
+      optional = true,
+      opts = function(_, opts)
+        if (vim.g.colors_name or ""):find("catppuccin") then
+          opts.highlights = require("catppuccin.special.bufferline").get_theme()
+        end
+      end,
+    },
   },
 }
 
