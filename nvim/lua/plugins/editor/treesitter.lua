d327229c086a75860aab5bf238af7d8c6700caea
Rodrigo Nascimento <rodrigo.nascimento@edu.udesc.br>
2026-05-25 14:23:44 -0300

    feat: updating to new nvim specs

--- nvim/lua/plugins/editor/treesitter.lua
+++ nvim/lua/plugins/editor/treesitter.lua
@@ -1,24 +1,48 @@
 return {
   "nvim-treesitter/nvim-treesitter",
+  branch = "main",
+  lazy = false,
   build = ":TSUpdate",
-  config = function ()
-    local config = require('nvim-treesitter.configs')
-    config.setup({
-      ensure_installed = {
+  config = function()
+    require("nvim-treesitter").setup()
+
+    require("nvim-treesitter").install({
+      "lua",
+      "javascript",
+      "fortran",
+      "markdown",
+      "markdown_inline",
+    })
+
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = {
         "lua",
         "javascript",
         "fortran",
         "markdown",
-        "markdown_inline",
       },
-      highlight = {
-        enable = true,
-        disable = { "latex", "tex" }
-      },
-      indent = {
-        enable = true
-      },
+      callback = function()
+        pcall(vim.treesitter.start)
+      end,
     })
-  end
-}
 
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = { "latex", "tex" },
+      callback = function()
+        vim.treesitter.stop()
+      end,
+    })
+
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = {
+        "lua",
+        "javascript",
+        "fortran",
+        "markdown",
+      },
+      callback = function()
+        vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+      end,
+    })
+  end,
+}
--- nvim/lua/plugins/langs-servers/lsp-config.lua
+++ nvim/lua/plugins/langs-servers/lsp-config.lua
@@ -1,10 +1,9 @@
-return {
+return{
   {
     "neovim/nvim-lspconfig",
     lazy = false,
-    config = function ()
-      local capabilities = require('cmp_nvim_lsp').default_capabilities()
-      -- local lspconfig = require('lspconfig')
+    config = function()
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
       local lspconfig = vim.lsp.config
       local icons = require("user-icons")
 
@@ -31,47 +30,47 @@ return {
         },
       })
 
-      vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = "rounded" })
-      vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, { border = "rounded" })
+      vim.o.winborder = "rounded"
       require("lspconfig.ui.windows").default_options.border = "rounded"
 
-      lspconfig('lua_ls',{
-        capabilities = capabilities
+      lspconfig("lua_ls", {
+        capabilities = capabilities,
       })
-      lspconfig('fortls',{
+
+      lspconfig("fortls", {
         capabilities = capabilities,
         cmd = {
-          'fortls',
-          '--notify_init',
-          '--hover_signature',
-          '--hover_language=fortran',
-          '--use_signature_help',
-          '--incremental_sync',
-          '--sort_keywords',
-          '--source_dirs',
-          '--autocomplete_no_snippets',
-          '--lowercase_intrinsics'
+          "fortls",
+          "--notify_init",
+          "--hover_signature",
+          "--hover_language=fortran",
+          "--use_signature_help",
+          "--incremental_sync",
+          "--sort_keywords",
+          "--source_dirs",
+          "--autocomplete_no_snippets",
+          "--lowercase_intrinsics",
         },
-        filetypes = { 'fortran' },
+        filetypes = { "fortran" },
         docs = {
           vscode = "hansec.fortran-ls",
           default_config = {
-            root_dir = [[root_pattern(".fortls")]]
-          }
+            root_dir = [[root_pattern(".fortls")]],
+          },
         },
         settings = {
           nthreads = 1,
         },
       })
-      -- lspconfig.ts_ls.setup({
-      --   capabilities = capabilities
-      -- })
-      -- lspconfig.html.setup({
-      --   capabilities = capabilities
-      -- })
-      -- lspconfig.cssls.setup({
-      --   capabilities = capabilities
-      -- })
-    end
-  }
+
+      vim.keymap.set("n", "K", function()
+        vim.lsp.buf.hover({ border = "rounded" })
+      end, { desc = "LSP Hover" })
+
+      vim.keymap.set("i", "<C-k>", function()
+        vim.lsp.buf.signature_help({ border = "rounded" })
+      end, { desc = "LSP Signature Help" })
+    end,
+  },
 }
+
